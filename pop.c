183d8852179be25f82347ebf12ab44431f101335
OmaymaK <emmablink050@gmail.com>
2022-05-12 14:28:05 -0700

    rja3na win konna

---
+++ pop.c
@@ -0,0 +1,21 @@
+#include "monty.h"
+/**
+ * _pop - remove element a list
+ *@stack: pointer to first node
+ *@line_number: integer
+ *Return: void
+ */
+void _pop(stack_t **stack, unsigned int line_number)
+{
+	stack_t *node = *stack;
+
+	if (stack == NULL || *stack == NULL)
+	{
+		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	*stack = node->next;
+	if (*stack != NULL)
+		(*stack)->prev = NULL;
+	free(node);
+}
