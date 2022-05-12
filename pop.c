c7cccc2ca746b37b9f2ed22089bafdac34756e7a
OmaymaK <emmablink050@gmail.com>
2022-05-12 14:48:33 -0700

    add func implementation

--- pop.c
+++ pop.c
@@ -5,7 +5,7 @@
  *@line_number: integer
  *Return: void
  */
-void _pop(stack_t **stack, unsigned int line_number)
+void pop(stack_t **stack, unsigned int line_number)
 {
 	stack_t *node = *stack;
 
