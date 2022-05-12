2292f5f9d8bcbcf91f7284ff0e5c274444fd7f25
jaafarfares <jaafarferes@gmail.com>
2022-05-12 11:26:06 +0100

    do nothying

---
+++ nop.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+/**
+ *nop - ....
+ *@stack: ....
+ *@line_number: ...
+ *
+ *Return: ....
+ */
+void nop(stack_t **stack, unsigned int line_number)
+{
+	(void) line_number;
+	(void) stack;
+}
