daa7843e0ce66cfe774233f84de652c1df72ca80
jaafarfares <jaafarferes@gmail.com>
2022-05-15 14:04:49 +0100

    test

--- main.c
+++ main.c
@@ -9,20 +9,19 @@ int main(int argc, char *argv[])
 {
 	FILE *file = NULL;
 	stack_t *stack = NULL;
-	int exit_code = EXIT_SUCCESS;
 
 	if (argc != 2)
 	{
 		fprintf(stderr, "USAGE: monty file\n");
-		return (exit_code);
+		return (EXIT_SUCCESS);
 	}
 	file = fopen(argv[1], "r");
 	if (file == NULL)
 	{
 		fprintf(stderr, "Error: Can't open file%s\n", argv[1]);
-		return (exit_code);
+		return (EXIT_SUCCESS);
 	}
 	execute_monty(file, &stack);
 	fclose(file);
-	return (exit_code);
+	return (EXIT_SUCCESS);
 }
