e2333a5e92554c7ad7b65e234b77f124b1db46fa
jaafarfares <jaafarferes@gmail.com>
2022-05-15 12:35:43 +0100

    test

---
+++ stack_queue.c
@@ -0,0 +1,23 @@
+#include "monty.h"
+/**
+ * stack2 - check the code
+ * @stack: number of arguments
+ * @line_number: arguments
+ * Return: Always EXIT_SUCCESS.
+ */
+void stack2(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
+/**
+ * queue2 - check the code
+ * @stack: number of arguments
+ * @line_number: arguments
+ * Return: Always EXIT_SUCCESS.
+ */
+void queue2(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
