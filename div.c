78277772e561498346311745dad37876e4812d33
OmaymaK <emmablink050@gmail.com>
2022-05-15 09:42:15 -0700

    div fix

--- div.c
+++ div.c
@@ -1,29 +1,29 @@
 #include "monty.h"
 /**
- * div - result of division of  top stack number with the next number
+ * divv - result of division of  top stack number with the next number
  * @stack: pointer to linked list
  * @line_number: line number
  */
-void div(stack_t **stack, unsigned int line_number)
+void divv(stack_t **stack, unsigned int line_number)
 {
 	int num = 0;
 
 	if (*stack == NULL || (*stack)->next == NULL)
 	{
 		fprintf(stderr, "L%u: can't div, stack too short\n", line_number);
-		_free(*stack);
+		_free(stack);
 		exit(EXIT_FAILURE);
 	}
-	else if (*stack->n == 0)
+	else if ((*stack)->n == 0)
 	{
 		fprintf(stderr, "L%d: division by zero\n", line_number);
-		_free(*stack);
+		_free(stack);
 		exit(EXIT_FAILURE);
 	}
 	else
 	{
-		num = *stack->n;
+		num = (*stack)->n;
 		pop(stack, line_number);
-		(*stack)->n / num = num;
+		(*stack)->n /= num;
 	}
 }
--- monty.h
+++ monty.h
@@ -58,7 +58,7 @@ void sub(stack_t **stack, unsigned int line_number);
 void rotl(stack_t **stack, unsigned int line_number);
 void rotr(stack_t **stack, unsigned int line_number);
 void pstr(stack_t **stack, unsigned int line_number);
-void div( stack_t **stack, unsigned int line_number);
+void divv( stack_t **stack, unsigned int line_number);
 
 
 #endif
