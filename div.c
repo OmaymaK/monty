d383b9d0eb1f168be5b0aae5419f9097712284a6
jaafarfares <jaafarferes@gmail.com>
2022-05-15 11:50:02 +0100

    div

--- div.c
+++ div.c
@@ -1,28 +1,26 @@
 #include "monty.h"
 /**
- * div - check the code
+ * _div - check the code
  * @stack: number of arguments
  * @line_number: arguments
  * Return: Always EXIT_SUCCESS.
  */
-void div(stack_t **stack, unsigned int line_number)
+void _div(stack_t **stack, unsigned int line_number)
 {
 	stack_t *tmp;
 
 	if (*stack == NULL || (*stack)->next == NULL)
 	{
 		fprintf(stderr, "L%d: can't div, stack too short\n", line_number);
-		free(vars.buffer);
-		free_list(stack);
-		fclose(vars.stream);
+		free(line);
+		_free(stack);
 		exit(EXIT_FAILURE);
 	}
 	if ((*stack)->n == 0)
 	{
 		fprintf(stderr, "L%d: division by zero\n", line_number);
-		free(vars.buffer);
-		free_list(stack);
-		fclose(vars.stream);
+		free(line);
+		_free(stack);
 		exit(EXIT_FAILURE);
 	}
 	tmp = *stack;
